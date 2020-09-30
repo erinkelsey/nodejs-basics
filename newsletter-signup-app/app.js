8295cf7b39d1b2bbce157f44cb00e8c1f76f9238
erinkelsey <erinszilagyi@gmail.com>
2020-09-30 15:43:42 -0600

    add a public folder for server static files

--- newsletter-signup-app/app.js
+++ newsletter-signup-app/app.js
@@ -3,6 +3,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
